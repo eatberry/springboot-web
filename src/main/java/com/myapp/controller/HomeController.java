04a0008cba8fe8a08119c8068e6127db31a19acc
eatberry <1009452821@qq.com>
2019-08-07 20:33:09 +0800

    sss

--- src/main/java/com/myapp/controller/HomeController.java
+++ src/main/java/com/myapp/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping("/mayan!imissyou")
     public String index() {
         return "mayan";
     }
